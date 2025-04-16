12b199a45c173645b82b7d76ed9658ebb4e7b262
Alberto Dal Dosso <alberto.daldosso@lbaconsulting.eu>
2025-04-16 18:04:33 +0200

    MongoDB connection

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
+const Activity = require("./models/Activity");
 
 const app = express();
 app.use(express.json());
@@ -9,6 +10,15 @@ app.use(cors());
 
 const STRAVA_CLIENT_ID = process.env.STRAVA_CLIENT_ID;
 const STRAVA_CLIENT_SECRET = process.env.STRAVA_CLIENT_SECRET;
+
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("✅ Connected to MongoDB"))
+  .catch((err) => console.error("❌ MongoDB connection error:", err));
 
 app.get("/strava/callback", async (req, res) => {
   const { code } = req.query;
@@ -79,6 +89,46 @@ app.post("/strava/refresh_token", async (req, res) => {
     res
       .status(500)
       .json({ error: "Failed to refresh token", details: error.response.data });
+  }
+});
+
+app.get("/strava/activities", async (req, res) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res
+      .status(401)
+      .json({ error: "Missing or invalid Authorization header" });
+  }
+
+  const accessToken = authHeader.split(" ")[1];
+
+  try {
+    const response = await axios.get(
+      "https://www.strava.com/api/v3/athlete/activities",
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+
+    const activities = response.data;
+
+    for (const activity of activities) {
+      await Activity.updateOne(
+        { id: activity.id },
+        { $set: activity },
+        { upsert: true }
+      );
+    }
+
+    res.json(activities);
+  } catch (error) {
+    console.error("Errore durante il recupero delle attività:", error.message);
+    res
+      .status(500)
+      .json({ error: "Errore durante il recupero delle attività" });
   }
 });
 
